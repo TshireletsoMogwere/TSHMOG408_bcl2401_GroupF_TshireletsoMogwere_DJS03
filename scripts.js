280e8ff8a75d80f74b1905699cdf58e67a01a05d
TshireletsoMogwere <tmogwere09@gmail.com>
2024-05-14 15:35:51 +0200

    added a function for preview button

--- scripts.js
+++ scripts.js
@@ -4,16 +4,12 @@ import { books, authors, genres, BOOKS_PER_PAGE } from './data.js'
 let page = 1;
 let matches = books
 
-// Creates a document fragment for iniitial book previews
-const starting = document.createDocumentFragment()
-
 // Iterates through matches and creates preview elements for first page
-for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
-    const element = document.createElement('button')
-    element.classList = 'preview'
-    element.setAttribute('data-preview', id)
-
-    // Populates the preview element with image, title and author information
+   function createPreviewButton(book) {
+ const { author, id, image, title } = book;
+    const element = document.createElement('button');
+    element.classList = 'preview';
+    element.setAttribute('data-preview', id);
     element.innerHTML = `
         <img
             class="preview__image"
@@ -24,11 +20,11 @@ for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
             <h3 class="preview__title">${title}</h3>
             <div class="preview__author">${authors[author]}</div>
         </div>
-    `
+    `;
 
-    // Appends preview elements to the document fragment
-    starting.appendChild(element)
+  return element;
 }
+
 
 // Appends initial previews to the document
 document.querySelector('[data-list-items]').appendChild(starting)
